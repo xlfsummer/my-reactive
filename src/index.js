a54b92e85a011d180d9dcbe3076809db548c94b9
xlfsummer <xlf-summer@outlook.com>
2020-12-26 16:52:56 +0800

    实现 watch, computed 依赖 computed

--- src/index.js
+++ src/index.js
@@ -36,6 +36,7 @@ export class Computed {
 
     #fn;
     #lastValue = Computed.#initValue;
+    #subscribers = new Set();
 
     constructor(fn){
         this.#fn = fn;
@@ -44,11 +45,24 @@ export class Computed {
     get value(){
         if(this.#lastValue == Computed.#initValue)
             this.evaluate();
+
+        this.addEvaluatingDependantToSubscriber();
+
         return this.#lastValue;
     }
 
     set value(v) {
         throw new SyntaxError("Can't set value of a Computed");
+    }
+
+    addEvaluatingDependantToSubscriber(){
+        let evaluatingDependant = evaluatingDependantStack[0];
+        if(!evaluatingDependant) return;
+        this.addSubscriber(evaluatingDependant);
+    }
+
+    addSubscriber(dependant){
+        this.#subscribers.add(dependant);
     }
 
     evaluate(){
@@ -62,5 +76,36 @@ export class Computed {
 
     notify(){
         this.evaluate();
+        for(let sub of this.#subscribers.values()) sub.notify();
+    }
+}
+
+export class Watcher {
+
+    #fn;
+    #cb;
+    #lastValue;
+
+    constructor(fn, cb){
+        this.#fn = fn;
+        this.#cb = cb;
+        this.evaluate();
+    }
+
+    evaluate(){
+        evaluatingDependantStack.unshift(this);
+        try {
+            this.#lastValue = this.#fn();
+        } finally {
+            evaluatingDependantStack.shift(this);
+        }
+    }
+
+    /** 监听的响应式值被改变通知 */
+    notify(){
+        let value = this.#fn();
+        if(Object.is(value, this.#lastValue)) return;
+        this.#cb(value, this.#lastValue);
+        this.#lastValue = value;
     }
 }
--- test/reactive.js
+++ test/reactive.js
@@ -1,7 +1,10 @@
-import { Reactive, Computed } from "../src/index.js";
+import { Reactive, Computed, Watcher } from "../src/index.js";
 
 let rWord = new Reactive("Hello");
-let cMessage = new Computed(() => "Saying:" + rWord.value);
+let cMessage = new Computed(() => "saying:" + rWord.value);
+let ccMessage = new Computed(() => "Bob is " + cMessage.value);
+new Watcher(() => cMessage.value, () => console.log("cMessage is changing"));
+new Watcher(() => ccMessage.value, () => console.log("ccMessage is changing"));
 
 console.log(rWord.value)
 console.log(cMessage.value)
@@ -10,4 +13,3 @@ rWord.value = "world"
 
 console.log(rWord.value)
 console.log(cMessage.value)
-
